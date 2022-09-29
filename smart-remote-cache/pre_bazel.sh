4159dbddd09f57403a048279f2113a3edbff68fa
Brandon Duffany <brandon@buildbuddy.io>
2022-09-29 16:08:38 -0400

    Redirect output to stderr

--- smart-remote-cache/pre_bazel.sh
+++ smart-remote-cache/pre_bazel.sh
@@ -4,7 +4,7 @@ for arg in "$@"; do
   echo "$arg"
 done
 
-if ! timeout 1 ping -c1 remote.buildbuddy.io; then
+if ! timeout 1 ping -c1 remote.buildbuddy.io &>/dev/null; then
     # Network is spotty; don't use cache
     echo "--remote_cache="
 fi
